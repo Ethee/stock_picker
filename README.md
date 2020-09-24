42c45c8e44c22eea0eddf79e3bf444ede2ed9033
Ethee <jakemmattern@gmail.com>
2020-09-24 17:51:00 -0400

    Fix bug when given array had smaller numbers towards the end of the array

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 ## Stock Picker
 
 A stock picker method that chooses the best day to buy and then sell from an array of options.
-
-### TODO:
-
-Fix bug if the smallest days come after the biggest in the array.
--- stock_picker.rb
+++ stock_picker.rb
@@ -1,20 +1,15 @@
-def stock_picker( arr[int] )
-  biggest_day = nil
-  smallest_day = nil
+def stock_picker( arr )
+  potential = []
+  difference = 0
   arr.each do |day|
-    if ( biggest_day = nil && smallest_day = nil )
-      biggest_day = day
-      smallest_day = day
-    else
-      if ( day > biggest_day )
-        biggest_day = day
-      elsif ( day < smallest_day )
-        smallest_day = day
+    arr.each do |match|
+      if ( arr.index(day) < arr.index(match) )
+        if ( day - match < difference )
+          difference = day - match
+          potential = [ arr.index(day), arr.index(match) ]
+        end
       end
     end
   end
-  picks = [ arr.index(smallest_day), arr.index(biggest_day) ]
-  puts picks
+  puts potential
 end
-
-stocks = []
